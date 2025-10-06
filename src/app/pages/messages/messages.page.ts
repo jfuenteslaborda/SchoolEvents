b0abc0fd0297273178f196844ddd1705c77a2fad
Jose Fuentes Laborda <jfuenteslaborda@safareyes.es>
2025-10-06 21:25:05 +0200

    adding functions on footer-menu

--- src/app/pages/messages/messages.page.ts
+++ src/app/pages/messages/messages.page.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {FooterMenuComponent} from "../../components/footer-menu/footer-menu.component";
+import {HeaderCalendarComponent} from "../../components/header-calendar/header-calendar.component";
+import {IonContent, IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
+import {HeaderMessagesComponent} from "../../components/header-messages/header-messages.component";
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.page.html',
-  styleUrls: ['./messages.page.scss'],
+    selector: 'app-messages',
+    templateUrl: './messages.page.html',
+    styleUrls: ['./messages.page.scss'],
+    imports: [
+        FooterMenuComponent,
+        HeaderCalendarComponent,
+        IonContent,
+        IonHeader,
+        IonTitle,
+        IonToolbar,
+        HeaderMessagesComponent
+    ]
 })
 export class MessagesPage implements OnInit {
 
