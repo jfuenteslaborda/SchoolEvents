c07eb8cee88021023fbb4c9e12d2cd06d654fd72
Jose Fuentes Laborda <jfuenteslaborda@safareyes.es>
2025-11-12 10:25:26 +0100

    adding backend file

---
+++ schoolevents_api/src/main/java/com/schoolevents/schoolevents_api/SchooleventsApiApplication.java
@@ -0,0 +1,13 @@
+package com.schoolevents.schoolevents_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooleventsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooleventsApiApplication.class, args);
+	}
+
+}
---
+++ schoolevents_api/src/test/java/com/schoolevents/schoolevents_api/SchooleventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolevents.schoolevents_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooleventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
