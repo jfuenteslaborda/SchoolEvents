58371eec905db7db1690a619e18619a355ee92a3
Jose Fuentes Laborda <jfuenteslaborda@safareyes.es>
2025-12-08 21:33:18 +0100

    Update SignService.java

--- schoolevents_api/src/main/java/com/schoolevents/schoolevents_api/Services/SignService.java
+++ schoolevents_api/src/main/java/com/schoolevents/schoolevents_api/Services/SignService.java
@@ -1,6 +1,7 @@
 package com.schoolevents.schoolevents_api.Services;
 
 import com.schoolevents.schoolevents_api.DTO.SignDTO;
+import com.schoolevents.schoolevents_api.exception.ElementNotFoundException;
 import com.schoolevents.schoolevents_api.mappers.SignMapper;
 import com.schoolevents.schoolevents_api.models.Sign;
 import com.schoolevents.schoolevents_api.repositories.SignRepository;
@@ -27,12 +28,16 @@ public class SignService {
         for (Sign sign : signs) {
             signsDTO.add(signMapper.signToSignDTO(sign));
         }
-        return signsDTO;
+        if (signs.isEmpty()){
+            throw new ElementNotFoundException("No hay comentarios registrados");
+        } else return signsDTO;
     }
 
     public SignDTO findId(Long id){
         Sign sign = signRepository.findById(id);
-        return signMapper.signToSignDTO(sign);
+        if (sign == null) {
+            throw new ElementNotFoundException("No hay registros con este id "+id);
+        } else return signMapper.signToSignDTO(sign);
     }
 
     public List<SignDTO> findByEventId(Long event_id){
@@ -41,7 +46,9 @@ public class SignService {
         for (Sign sign : signs) {
             signsDTO.add(signMapper.signToSignDTO(sign));
         }
-        return signsDTO;
+        if (signs.isEmpty()){
+            throw new ElementNotFoundException("No hay comentarios registrados para el evento con el id: "+event_id);
+        } else return signsDTO;
     }
 
     public List<SignDTO> findByUserId(Long user_id){
@@ -50,24 +57,40 @@ public class SignService {
         for (Sign sign : signs) {
             signsDTO.add(signMapper.signToSignDTO(sign));
         }
-        return signsDTO;
+        if (signs.isEmpty()){
+            throw new ElementNotFoundException("No hay comentarios registrados para el usuario con el id: "+user_id);
+        } else return signsDTO;
     }
 
     public SignDTO save(Sign sign){
-        Sign s = signRepository.save(sign);
-        return signMapper.signToSignDTO(s);
+        if (sign == null) {
+            throw new ElementNotFoundException("No se puede guardar un comentario vacio");
+        } else {
+            Sign s = signRepository.save(sign);
+            return signMapper.signToSignDTO(s);
+        }
     }
 
     public SignDTO update(Sign sign, Long id){
-        Sign s = signRepository.findById(id);
-        s.setEvent(s.getEvent());
-        s.setUser(sign.getUser());
-        s.setDate(sign.getDate());
-        Sign s0 = signRepository.save(s);
-        return signMapper.signToSignDTO(s0);
+        if (sign == null) {
+            throw new ElementNotFoundException("No se puede actualizar un comentario vacio");
+        } else {
+            if (signRepository.findById(id) == null) {
+                throw new ElementNotFoundException("No se puede actualizar un comentario sin id");
+            } else {
+                Sign s = signRepository.findById(id);
+                s.setEvent(s.getEvent());
+                s.setUser(sign.getUser());
+                s.setDate(sign.getDate());
+                Sign s0 = signRepository.save(s);
+                return signMapper.signToSignDTO(s0);
+            }
+        }
     }
 
     public void delete(Long id){
-        signRepository.deleteById(id);
+        if (signRepository.findById(id) == null) {
+            throw new ElementNotFoundException("No se puede eliminar un comentario sin id");
+        } else signRepository.deleteById(id);
     }
 }
