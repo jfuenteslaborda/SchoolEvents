565beb9aed56eb1d13f31c75e3af841e3e83e61f
Jose Fuentes Laborda <jfuenteslaborda@safareyes.es>
2025-12-08 21:36:21 +0100

    Update UserService.java

--- schoolevents_api/src/main/java/com/schoolevents/schoolevents_api/Services/UserService.java
+++ schoolevents_api/src/main/java/com/schoolevents/schoolevents_api/Services/UserService.java
@@ -2,6 +2,7 @@ package com.schoolevents.schoolevents_api.Services;
 
 import com.schoolevents.schoolevents_api.DTO.UserDTO;
 import com.schoolevents.schoolevents_api.DTO.UserStadisticsDTO;
+import com.schoolevents.schoolevents_api.exception.ElementNotFoundException;
 import com.schoolevents.schoolevents_api.mappers.UserMapper;
 import com.schoolevents.schoolevents_api.models.*;
 import com.schoolevents.schoolevents_api.repositories.UserRepository;
@@ -28,41 +29,63 @@ public class UserService {
         for (User user : users){
             usersDTO.add(userMapper.userToUserDTO(user));
         }
-        return usersDTO;
+        if (users.isEmpty()){
+            throw new ElementNotFoundException("No hay usuarios registrados");
+        } else return usersDTO;
     }
 
     public UserDTO findById(Long id){
         User user = userRepository.findById(id);
-        return userMapper.userToUserDTO(user);
+        if (user == null) {
+            throw new ElementNotFoundException("Usuario no encontrado con el id: "+id);
+        } else return userMapper.userToUserDTO(user);
     }
 
     public UserStadisticsDTO getUserStadistics(){
-        return userRepository.getUserStadistic();
+        if (userRepository.getUserStadistic() == null){
+            return new UserStadisticsDTO();
+        } else return userRepository.getUserStadistic();
     }
 
     public UserDTO findByEmail(String email){
         User user = userRepository.findByEmail(email);
-        return userMapper.userToUserDTO(user);
+        if (user == null) {
+            throw new ElementNotFoundException("Usuario no encontrado con el email: "+email);
+        } else return userMapper.userToUserDTO(user);
     }
 
     public UserDTO save(User user){
-        User u = userRepository.save(user);
-        return userMapper.userToUserDTO(u);
+        if (user == null) {
+            throw new ElementNotFoundException("No se puede guardar un usuario vacio");
+        } else {
+            User u = userRepository.save(user);
+            return userMapper.userToUserDTO(u);
+        }
     }
 
     public UserDTO update(User user, Long id){
-        User u = userRepository.findById(id);
-        u.setEmail(user.getEmail());
-        u.setDate(user.getDate());
-        u.setPassword(user.getPassword());
-        u.setPhoto(user.getPhoto());
-        u.setFull_name(user.getFull_name());
-        u.setIs_Admin(user.getIs_Admin());
-        User u0 = userRepository.save(u);
-        return userMapper.userToUserDTO(u0);
+        if (user == null) {
+            throw new ElementNotFoundException("No se puede actualizar un usuario vacio");
+        } else {
+            if (userRepository.findById(id) == null) {
+                throw new ElementNotFoundException("Usuario no encontrado con el id: "+id);
+            } else {
+                User u = userRepository.findById(id);
+                u.setEmail(user.getEmail());
+                u.setDate(user.getDate());
+                u.setPassword(user.getPassword());
+                u.setPhoto(user.getPhoto());
+                u.setFull_name(user.getFull_name());
+                u.setIs_Admin(user.getIs_Admin());
+                User u0 = userRepository.save(u);
+                return userMapper.userToUserDTO(u0);
+            }
+        }
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        if (userRepository.findById(id) == null) {
+            throw new ElementNotFoundException("No se puede eliminar un usuario sin id");
+        } else userRepository.deleteById(id);
     }
 }
